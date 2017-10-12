24b237fc719d9ef044a54178d8d0bd571221c147
oddgas <oddgas@stud.ntnu.no>
2017-10-12 09:38:20 +0200

    cleaned up some changes

--- Personal Projects/stockAnalyzerProgram/StringDivider.java
+++
@@ -1,8 +0,0 @@
-package stockAnalyzerProgram;
-
-public class StringDivider {
-
-	public StringDivider(){
-		
-	}
-}
--- Personal Projects/stockAnalyzerProgram/TickerReader.java
+++
@@ -1,5 +0,0 @@
-package stockAnalyzerProgram;
-
-public class TickerReader {
-
-}
